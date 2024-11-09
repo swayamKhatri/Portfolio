c22b5d49ee7b184d27aa9709f4fc406319eea124
swayamKhatri <khatriswayam70@gmail.com>
2024-11-10 01:13:30 +0530

    Add .gitignore file

---
+++ Portfolio/Portfolio_Swayam/src/components/About/About.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/ladka.jpg")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Symbiosis Institute of Technology (2022-2026)</h3>
+              <p>
+                Pursuing my B.TECH in Computer Science Department. 
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Edunova-12th (2021-2022) </h3>
+              <p>
+                Completed PCM and learnt about computer basics.
+              </p>
+            </div>
+          </li> 
+        </ul>
+      </div>
+    </section>
+  );
+};
